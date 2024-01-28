656c00721acacb5884ad1cd1d5d8260e25cdc3a7
Malgorzata Ciesielska <s26974@pjwstk.edu.pl>
2024-01-28 11:49:59 +0100

    initial commit

---
+++ src/main/java/pl/pjatk/s26974_Bank/S26974BankApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s26974_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26974BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S26974BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/s26974_Bank/S26974BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s26974_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S26974BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
